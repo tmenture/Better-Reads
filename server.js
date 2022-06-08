cd5662e908ee7125e89d64b6e0153e1739b602bb
Liz DiTullio <lizdit72296@gmail.com>
2022-06-08 18:13:56 -0400

    created homepage and dashboard

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-
-// Imports Sequelize connection
-const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
-const PORT = process.env.JAWSDB || 3001;
+const PORT = process.env.PORT || 3001;
 
-// Midleware 
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers'));
 
-// Syncs sequelize modules to the database and turns on the server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  app.listen(PORT, () => console.log(`Now listening at ${PORT}`));
 });

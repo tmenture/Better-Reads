305b81d0db8316fdef4c87b1b3793c405535ef74
Liz DiTullio <lizdit72296@gmail.com>
2022-06-13 15:23:52 -0400

    google books api working and console logging data

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const bookRoutes = require('./book-routes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/search', bookRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
--- public/Javascript/search-books.js
+++ public/Javascript/search-books.js
@@ -1,29 +1,52 @@
-function getBooks(event) {  
-    event.preventDefault();
-    let api = "https://www.googleapis.com/books/v1/volumes?q=harrypotter"
-    console.log(api)
-     fetch(api)
-    .then(function(response){
-        var books = response.data.items
-            var booksArr = [];
-            for (var i = 0; i < books.length; i++) {
-                var image;
-                if (!bookInfo.imageLinks) {
-                    image = "https://via.placeholder.com/300/400";
-                } else {
-                    image = bookInfo.imageLinks.thumbnail;
-                }
-                booksArr.push({
-                    title: bookInfo.title,
-                    author: bookInfo.authors,
-                    description: bookInfo.description,
-                    id: books[i].id,
-                    image: image,
-                });
-            }
-    }).catch(function(error){
-        console.log(error)
-    });
-};
 
-document.querySelector('.search-btn').addEventListener('click', getBooks());
+var getBooks = function (event) {
+    event.preventDefault();
+
+    const bookInput = document.querySelector('input[type="search"]').value;
+    console.log(bookInput);
+    const api = ("https://www.googleapis.com/books/v1/volumes?q=" + bookInput)
+
+    fetch(api)
+
+       .then((response) => {
+
+           if (response.ok) {
+
+               return response.json();
+
+           } else {
+
+               throw new Error("Network Response Error");
+
+           }
+       })
+
+       .then(data => {
+
+       console.log(data);
+
+       sendToDataBase(data);
+
+   })
+
+   .catch((error) => console.error("Fetch Error:", error));
+
+}
+
+
+sendToDataBase = function(response) {
+    var books = response.items
+    var booksArr = [];
+   for (var i = 0; i < books.length; i++) {
+       booksArr.push({
+          title: books[i].volumeInfo.title,
+          author: books[i].volumeInfo.authors,
+          description: books[i].volumeInfo.description,
+          id: books[i].id,
+        });
+    }
+    console.log(booksArr);
+}
+
+document.querySelector('.book-form').addEventListener('submit', getBooks);
+

decb16639ec6ce9c3b8fee202f1adb98ce8090a1
Liz DiTullio <lizdit72296@gmail.com>
2022-06-13 19:19:20 -0400

    displaying book take 1

--- public/Javascript/search-books.js
+++ public/Javascript/search-books.js
@@ -24,7 +24,7 @@ var getBooks = function (event) {
 
        console.log(data);
 
-      sendToDataBase(data);
+      displayBooks(data);
 
    })
 
@@ -33,33 +33,34 @@ var getBooks = function (event) {
 }
 
 
-sendToDataBase = function(response) {
-  console.log("you made it to send to database")
-    var books = response.items
+displayBooks = function(response) {
+  
+   const bookShelfEl = response.items
     var booksArr = [];
    for (var i = 0; i < books.length; i++) {
       booksArr.push({
          title: books[i].volumeInfo.title,
-          author: books[i].volumeInfo.authors,
-          description: books[i].volumeInfo.description,
+          author: books[i].volumeInfo.authors[0],
+         // description: books[i].volumeInfo.description,
           id: books[i].id,
         });
     }
-    searchFormHandler(booksArr);
 }
 
 
-
-async function searchFormHandler(booksArr) {
+// button to do this on click 
+async function SaveBook(event) {
   console.log("you made it to search form handler")
   for( var i =0; i<booksArr.length; i++) {
-    const title = booksArr[i].title;
-    const author = booksArr[i].author;
+    const title = $(this).title;
+    const author = $(this).author;
+    const id = booksArr[i].id;
     const response = await fetch(`/search`, {
         method: 'POST',
         body: JSON.stringify({
             title,
-            author
+            author,
+            //description
         }),
         headers: {
             'Content-Type': 'application/json'
@@ -67,7 +68,7 @@ async function searchFormHandler(booksArr) {
       });
       if (response.ok) {
         console.log("heading to a new page")
-       // document.location.reload();
+       document.location.reload();
       } else {
         alert(response.statusText);
       }
@@ -76,6 +77,8 @@ async function searchFormHandler(booksArr) {
 
 
 document.querySelector('.book-form').addEventListener('submit', getBooks);
+
+document.querySelector('.save-book').addEventListener('click',saveBook);
 
 
 
--- seeds/book-seeds.js
+++ seeds/book-seeds.js
@@ -1,14 +1,6 @@
 const { Book } = require('../models');
 
 const bookdata = [
-  {
-    title: "The Bible",
-    author: 'God',
-  },
-  {
-    title: "Harry Potter",
-    author: 'J.K. Rowling',
-  },
 ];
 
 const seedBooks = () => Book.bulkCreate(bookdata);

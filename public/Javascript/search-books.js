af2e6b294b980172384d8e5f5f56362f8a019355
Liz DiTullio <lizdit72296@gmail.com>
2022-06-13 16:44:50 -0400

    This is my best guess at how to add to a database

--- public/Javascript/search-books.js
+++ public/Javascript/search-books.js
@@ -1,4 +1,3 @@
-
 var getBooks = function (event) {
     event.preventDefault();
 
@@ -25,7 +24,7 @@ var getBooks = function (event) {
 
        console.log(data);
 
-       sendToDataBase(data);
+      sendToDataBase(data);
 
    })
 
@@ -35,18 +34,51 @@ var getBooks = function (event) {
 
 
 sendToDataBase = function(response) {
+  console.log("you made it to send to database")
     var books = response.items
     var booksArr = [];
    for (var i = 0; i < books.length; i++) {
-       booksArr.push({
-          title: books[i].volumeInfo.title,
+      booksArr.push({
+         title: books[i].volumeInfo.title,
           author: books[i].volumeInfo.authors,
           description: books[i].volumeInfo.description,
           id: books[i].id,
         });
     }
-    console.log(booksArr);
+    searchFormHandler(booksArr);
 }
 
+
+
+async function searchFormHandler(booksArr) {
+  console.log("you made it to search form handler")
+  for( var i =0; i<booksArr.length; i++) {
+    const title = booksArr[i].title;
+    const author = booksArr[i].author;
+    const response = await fetch(`/search`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            author
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      });
+      if (response.ok) {
+        console.log("heading to a new page")
+       // document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+  }
+}
+
+
 document.querySelector('.book-form').addEventListener('submit', getBooks);
+
+
+
+
+
 

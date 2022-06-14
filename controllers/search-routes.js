b8f8fb0820e0d8b446b9be31524892c0cc569480
Liz DiTullio <lizdit72296@gmail.com>
2022-06-14 17:58:27 -0400

    actually got the api call working

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Review, Book } = require('../models');
 const withAuth = require('../utils/auth');
+const axios = require('axios')
 
 router.get("/", (req, res) => {
-    res.render("search-books");
-});
+    const api = ("https://www.googleapis.com/books/v1/volumes?q=" + req.query.book)
+    console.log(api)
+    axios.get(api).then(data=> { 
+    res.render("search-books", {
+        books: data.data.items
+         }
+    )
+    console.log(data.data.items)
+})
 
-
-
-
-router.post("/", (req, res) => {
-    res.render("search-books", {book_title: req.body.book_title, book_author: req.body.book_author, book_desc: req.body.book_desc} );
-    console.log(req.body.book_title)
-});
+})
 
 module.exports = router;
-

0951c76663c489af5a4fe2fec4155f66052ddaed
Liz DiTullio <lizdit72296@gmail.com>
2022-06-14 20:28:16 -0400

    should be an mvp

--- seeds/review-seeds.js
+++ seeds/review-seeds.js
@@ -1,7 +1,66 @@
 const { Review } = require('../models');
 
 const reviewdata = [
-
+    {
+        title: "This book",
+        review_content: 'Wow love this book',
+        user_id: 1,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      },
+      {
+        title: 'Another Book',
+        review_content: 'This book was fine',
+        user_id: 2,
+      }
 ];
 
 const seedReviews = () => Review.bulkCreate(reviewdata);

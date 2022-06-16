6c86f2723760e0c460c105123f1ddd586ae80bfe
tmenture <thomasoxemail@gmail.com>
2022-06-16 13:33:11 -0400

    save book button works opnly for the first card

--- controllers/api/book-routes.js
+++ controllers/api/book-routes.js
@@ -1,9 +1,74 @@
 const router = require('express').Router();
 const axios = require('axios');
-const { Book } = require('../../models');
+const { Book, User, Review } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, (req, res) => {
+router.get('/', (req, res) => {
+    Book.findAll({
+        attributes: [
+            'id',
+            'title',
+            'author',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE review.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Review,
+                attributes: ['id', 'title', 'review_content', 'user_id', 'book_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbBookData => res.json(dbBookData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Book.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'author'
+        ],
+        include: [
+            {
+                model: Review,
+                attributes: ['id', 'title', 'review_content', 'user_id', 'book_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbBookData => {
+        if (!dbBookData) {
+            res.status(404).json({ message: 'No book found with this id' });
+            return;
+        }
+        res.json(dbBookData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/dashboard', withAuth, (req, res) => {
     Book.create({
         id: req.body.id,
         title: req.body.title,
@@ -15,7 +80,7 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-router.delete('/:id', withAuth, (req, res) => {
+router.delete('/dashboard/:id', withAuth, (req, res) => {
     Book.destroy({
         where: {
             id: req.params.id
--- public/Javascript/save-book.js
+++ public/Javascript/save-book.js
@@ -1,25 +1,7 @@
 function saveBook(req, res) {
+    const bookInfo = document.querySelector('.card-header').textContent;
 
-    const bookInfo = document.querySelector('.book-info');
-    // const author = document.querySelector('.book-author');
-
-    const url = "https://www.googleapis.com/books/v1/volumes?q=" + req.query;
-
-   fetch(url).then((res) => res.json()).then(function(data) {
-    let savedBook = data.results;
-    savedBook.map(function(books) {
-        let list = document.createElement('li');
-        list.innerHTML = `${books.index}`;
-
-        bookInfo.appendChild(list);
-    })
-   })
-
-    if (response.ok) {
-        response.render('saved-books.handlebars',);
-    } else {
-        alert(response.statusText);
-    }
+    console.log(bookInfo);
 }
 
 document.querySelector('.saveBtn').addEventListener('click', saveBook);

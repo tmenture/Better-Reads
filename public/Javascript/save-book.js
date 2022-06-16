35156d69d1d585fc2aa84ce587a75d5427af651f
tmenture <thomasoxemail@gmail.com>
2022-06-16 18:56:42 -0400

    altered variables

--- public/Javascript/save-book.js
+++ public/Javascript/save-book.js
@@ -1,10 +1,12 @@
-function saveBook() {
-    const bookInfo = document.querySelector('.card-header').textContent;
+function saveBook(event) {
+    event.preventDefault();
+
+    let bookInfo = document.querySelector('.card-header').textContent;
 
     console.log(bookInfo);
 }
 
-const buttons = document.querySelectorAll('.saveBtn');
+let buttons = document.querySelectorAll('.saveBtn');
 
 buttons.forEach((button) => {
     button.addEventListener('click', saveBook);
